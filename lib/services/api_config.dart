20afb32f9e97ea839e745a5472b6224f897b3d76
egurchenkovwallstreet-sys <e.gurchenkov.wallstreet@gmail.com>
2026-06-16 17:02:27 +0300

    Update api_config.dart

--- lib/services/api_config.dart
+++ lib/services/api_config.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/foundation.dart';
 
 /// Адрес backend-сервера.
-/// Chrome / Windows: localhost. Android-эмулятор: замените на http://10.0.2.2:3000
+/// Локально: localhost. Сервер Timeweb: см. [remoteHost].
 class ApiConfig {
   ApiConfig._();
 
   static const int port = 3000;
 
+  /// IP сервera Timeweb. Пустая строка = только localhost (ПК без сервера).
+  static const String remoteHost = '5.129.243.246';
+
   static String get baseUrl {
+    if (remoteHost.isNotEmpty) {
+      return 'http://$remoteHost:$port';
+    }
+
     if (kIsWeb) {
       final host = Uri.base.host;
       if (host != 'localhost' && host != '127.0.0.1') {
